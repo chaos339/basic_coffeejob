bab9a3d8a25d92d6ba672f9b539dd8f3116615f9
chaos339 <86054048+chaos339@users.noreply.github.com>
2021-06-18 00:01:30 +0800

    Add files via upload

---
+++ coffeejob.sql
@@ -0,0 +1,9 @@
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+('dough', 'Dough', 1, 0, 1),
+('sugar', 'Sugar', 1, 0, 1),
+('chocolate_chips', 'Chocolate Chips', 1, 0, 1),
+('coffee_bean', 'Coffee Bean(1g)', 2, 0, 1),
+('bm_donut', 'Holy Donut', 1, 0, 1),
+('bm_cookie', 'Cookie Ni Lisa', 1, 0, 1),
+('bm_coffee', 'Coffee', 1, 0, 1),
+('bm_frappe', 'Frappukeano', 1, 0, 1),
